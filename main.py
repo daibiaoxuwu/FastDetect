08c2517548d90aa946bc31888498019bd0cdcb2b
daibiaoxuwu <dujluo@gmail.com>
2025-09-21 17:44:17 -0400

    new work

--- main.py
+++ main.py
@@ -9,9 +9,9 @@ from work_new import work_new
 if Config.sf>=11: logger.warning(f"WARNING: SF={Config.sf}, LDRO might be enabled.")
 
 if __name__ == "__main__":
-    # work_new(-40971.948630148894, 0.01015366531, "data/test_1226")
     fstart = -40971.948630148894
-    tstart =  4240270.873306715
-    work(fstart, 0, "data/test_1226")
+    tstart =  4240090.873306715
+    # work(fstart, 0, "data/test_1226")
+    work_new(fstart, tstart, "data/test_1226")
 
 # %%
--- work_new.py
+++ work_new.py
@@ -5,25 +5,41 @@ from reader import SlidingComplex64Reader
 from matplotlib import pyplot as plt
 
 
-def work_new(estf, estt, file_path):
+def work_new(fstart, tstart, file_path):
     file_size = os.path.getsize(file_path)
     complex64_size = xp.dtype(xp.complex64).itemsize
     assert complex64_size == 8
     print(f"{file_path=} Size in Number of symbols: {file_size // complex64_size // Config.nsamp}")
 
     reader = SlidingComplex64Reader(file_path)
+    nsamp_small_2 = 2 ** Config.sf / Config.bw * Config.fs * (1 - fstart / Config.sig_freq)
 
-    tsymblen = 2 ** Config.sf / Config.bw * (1 - estf / Config.sig_freq)
-    coeff = xp.array((0, estf))
-    coeft = xp.array((tsymblen, estt))
+    start_pos = around(tstart - 100)
+    length = 200
+    pltfig1(to_host(xp.arange(start_pos, start_pos + length)), to_host(xp.unwrap(xp.angle(reader.get(start_pos, length)))), addvline=(tstart,)).show()
 
-    sig = reader.get(around(estt * Config.fs) - Config.nsamp, Config.nsamp * 400)
-    cfosymb = xp.exp(2j * xp.pi * estf * xp.linspace(0, (len(sig) - 1) / Config.fs, num=len(sig)))
-    cfosymb = cfosymb.astype(xp.complex64)
+    tstart += nsamp_small_2 * 120
+    start_pos = around(tstart - 100)
+    print("Avg Amplitude:", xp.mean(xp.abs(reader.get(start_pos, length))))
+    pltfig1(to_host(xp.arange(start_pos, start_pos + length)), to_host(xp.unwrap(xp.angle(reader.get(start_pos, length)))), addvline=(tstart,)).show()
 
-    plt.plot(to_host(xp.unwrap(xp.angle(sig))))
-    plt.plot(to_host(xp.unwrap(xp.angle(cfosymb))))
-    plt.plot(to_host(xp.unwrap(xp.angle(sig * xp.conj(cfosymb)))))
-    plt.axvline(Config.nsamp * 240)
-    plt.title("raw signal phase")
-    plt.show()
+    length = around(nsamp_small_2 + 200)
+    sig = reader.get(start_pos, length)
+    cfosymb = xp.exp(2j * xp.pi * fstart * xp.linspace(0, (len(sig) - 1) / Config.fs, num=len(sig)))
+    cfosymb = xp.unwrap(xp.angle(cfosymb)).astype(xp.float32)
+    sig = xp.unwrap(xp.angle(sig))
+    cfosymb += sig[100] - cfosymb[100]
+    fig = pltfig1(to_host(xp.arange(start_pos, start_pos + length)), to_host(sig), addvline=(tstart, tstart + nsamp_small_2))
+    pltfig1(to_host(xp.arange(start_pos, start_pos + length)), to_host(cfosymb), fig=fig).show()
+
+    length = 200
+    tstart += nsamp_small_2 * 120
+    start_pos = around(tstart - 100)
+    pltfig1(to_host(xp.arange(start_pos, start_pos + length)), to_host(xp.unwrap(xp.angle(reader.get(start_pos, length)))), addvline=(tstart,)).show()
+
+    tstart -= nsamp_small_2 * 240
+    
+    tsymblen = 2 ** Config.sf / Config.bw * (1 - fstart / Config.sig_freq)
+    coeff = xp.array((0, fstart))
+    coeft = xp.array((tsymblen, tstart))
+
